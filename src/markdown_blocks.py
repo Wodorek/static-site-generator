4d993591a5a34395c9b992dee46f6279af77c0de
Wodorek <krol.w94@gmail.com>
2024-08-27 20:37:21 +0200

    added block to block type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,11 @@
+block_type_heading = 'heading'
+block_type_paragraph = 'paragraph'
+block_type_code = 'code'
+block_type_quote = 'quote'
+block_type_ulist = 'ulist'
+block_type_olist = 'olist'
+
+
 def markdown_to_blocks(markdown):
 
     blocks = markdown.split('\n\n')
@@ -15,3 +23,55 @@ def markdown_to_blocks(markdown):
     blank_removed = remove_blank(stripped)
 
     return blank_removed
+
+
+def block_to_block_type(block):
+
+    lines = block.split('\n')
+
+    if (block.startswith('# ')
+            or block.startswith('## ')
+            or block.startswith('### ')
+            or block.startswith('#### ')
+            or block.startswith('##### ')
+            or block.startswith('###### ')):
+
+        return block_type_heading
+
+    if (lines[0].startswith("```") and lines[-1].startswith("```")):
+
+        return block_type_code
+
+    if block.startswith('>'):
+        for line in lines:
+            if not line.startswith('>'):
+                return block_type_paragraph
+
+        return block_type_quote
+
+    if block.startswith('* '):
+        for line in lines:
+            if not line.startswith('* '):
+                return block_type_paragraph
+
+        return block_type_ulist
+
+    if block.startswith('- '):
+        for line in lines:
+            if not line.startswith('- '):
+                return block_type_paragraph
+
+        return block_type_ulist
+
+    if block.startswith('1. '):
+
+        i = 1
+
+        for line in lines:
+            if not line.startswith(f'{i}. '):
+                return block_type_paragraph
+            i += 1
+
+        return block_type_olist
+
+    return block_type_paragraph
