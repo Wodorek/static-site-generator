1262519a1a0f7f8d65e88316324a6e5d9063b487
Wodorek <krol.w94@gmail.com>
2024-08-28 13:44:30 +0200

    added copy function

--- src/main.py
+++ src/main.py
@@ -1,10 +1,48 @@
-from textnode import TextNode
+import os
+import shutil
+
+
+def clear_folder(dest):
+    for filename in os.listdir(dest):
+        filepath = os.path.join(dest, filename)
+        try:
+            shutil.rmtree(filepath)
+        except OSError:
+            os.remove(filepath)
+
+
+def copy_to_directory(curr_path, src, destination):
+
+    clear_folder(destination)
+
+    path_from = os.path.join(curr_path, src)
+
+    items = os.listdir(path_from)
+
+    for item in items:
+
+        path_to_item = os.path.join(path_from, item)
+
+        if os.path.isdir(path_to_item):
+
+            path_to_dest = os.path.join(destination, item)
+
+            print(path_to_dest)
+
+            if not os.path.exists(path_to_dest):
+                os.mkdir(path_to_dest)
+
+            copy_to_directory(path_to_item, '',
+                              os.path.join(destination, item))
+
+        else:
+            shutil.copy(path_to_item, destination)
 
 
 def main():
-    node = TextNode('A node', 'bold', 'https://google.com')
 
-    print(node.__repr__())
+    copy_to_directory(os.path.abspath(''), 'static',
+                      os.path.join(os.path.abspath(''), 'public'))
 
 
 if __name__ == '__main__':
